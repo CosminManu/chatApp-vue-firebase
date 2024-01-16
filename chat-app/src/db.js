8e6434b2a612ce7d208f663d89ed3228d2b0b607
CosminManu <cosmin.manu2000@gmail.com>
2024-01-16 16:37:18 +0200

    initialized firebase

---
+++ chat-app/src/db.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+import "firebase/database"
+
+const config = {
+    apiKey: "AIzaSyC-bSPatxgAyv2PKbwwio80yR_48cYpDt0",
+    authDomain: "vuechat-tylerpotts.firebaseapp.com",
+    projectId: "vuechat-tylerpotts",
+    storageBucket: "vuechat-tylerpotts.appspot.com",
+    messagingSenderId: "676737726511",
+    appId: "1:676737726511:web:68b90e68272c15f9e5eeda"
+}
+
+const db = firebase.initializeApp(config)
+export default db
